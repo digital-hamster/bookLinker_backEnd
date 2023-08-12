3cd5e5819336c95cd0fc2c7d1c8b4ef37c6f3372
digital-hamster <waterlove1439@naver.com>
2023-08-12 21:32:51 +0900

    [setting] master Setting

---
+++ src/main/java/com/books_recommend/book_recommend/BookRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.books_recommend.book_recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books_recommend/book_recommend/BooksRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books_recommend.book_recommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
