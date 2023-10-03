f7033ddc6bfa3241485505b650fc5896b217d044
digital-hamster <waterlove1439@naver.com>
2023-10-03 17:30:31 +0900

    컨트롤러 수정

--- src/main/java/com/books_recommend/book_recommend/auth/controller/AuthenticationController.java
+++ src/main/java/com/books_recommend/book_recommend/auth/controller/AuthenticationController.java
@@ -4,19 +4,17 @@ import com.books_recommend.book_recommend.auth.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(("/members/authenticate"))
 @CrossOrigin
 @RequiredArgsConstructor
 class AuthenticationController {
 
     private final AuthenticationService service;
 
-    @PostMapping("/members/authenticate")
+    @PostMapping
     public ResponseEntity createAuthenticationToken(@RequestBody JwtRequest request){
         var requirement = new AuthenticationService.Requirement(
             request.email,
