bec1366ce968d7789684067a3e5753bf64f7603c
digital-hamster <waterlove1439@naver.com>
2023-10-03 16:06:31 +0900

    전체 로직 테스트 및 예외 추가 - 이메일 형식이 아닙니다

--- src/main/java/com/books_recommend/book_recommend/controller/MemberController.java
+++ src/main/java/com/books_recommend/book_recommend/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.books_recommend.book_recommend.controller;
 import com.books_recommend.book_recommend.common.web.ApiResponse;
 import com.books_recommend.book_recommend.dto.MemberDto;
 import com.books_recommend.book_recommend.service.MemberService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +16,7 @@ class MemberController {
     private final MemberService service;
 
     @PostMapping
-    ApiResponse<Response> createMember(@RequestBody Request request){
+    ApiResponse<Response> createMember(@RequestBody @Valid Request request){
         MemberDto memberDto = service.createMember(request.toRequirement());
 
         Response response =  new Response(memberDto.id());
@@ -22,7 +24,8 @@ class MemberController {
     }
 
     record Request(
-            @NotBlank(message = "email는 필수입니다.")
+            @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+                , message = "올바른 이메일 형식이 아닙니다.")
             String email,
             @NotBlank(message = "nickname는 필수입니다.")
             String nickname,
@@ -38,12 +41,6 @@ class MemberController {
     record Response(
             Long id
     ){}
-
-
-//            @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
-//                    , message = "올바른 이메일 형식이 아닙니다.")
-//            @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$",
-//                    message = "올바른 전화번호 형식을 기입해주세요.")
 
     @PutMapping("/update")
     ApiResponse updateMember(@RequestBody PutRequest putRequest){
