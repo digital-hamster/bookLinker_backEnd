3b728dcbe64547aeae7699f8e1322479358c372e
digital-hamster <waterlove1439@naver.com>
2023-10-11 18:48:45 +0900

    feat_favorite - 좋아요 전체 컨트롤러 매칭

--- src/main/java/com/books_recommend/book_recommend/controller/ListFavoriteController.java
+++ src/main/java/com/books_recommend/book_recommend/controller/ListFavoriteController.java
@@ -1,11 +1,13 @@
 package com.books_recommend.book_recommend.controller;
 
+import com.books_recommend.book_recommend.common.web.ApiResponse;
+import com.books_recommend.book_recommend.dto.ListFavoriteDto;
 import com.books_recommend.book_recommend.service.ListFavoriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/favorite")
@@ -14,13 +16,63 @@ class ListFavoriteController {
     private final ListFavoriteService service;
 
     @PostMapping("/{bookListId}")
-    public PostResponse create(@PathVariable Long bookListId){
-        service.create(bookListId);
+    public ApiResponse<PostResponse> create(@PathVariable Long bookListId){
+        var dto = service.create(bookListId);
+        var response = new PostResponse(
+            dto.id()
+        );
 
-
+        return ApiResponse.success(response);
     }
 
     record PostResponse(
         Long id
+    ){}
+
+    @DeleteMapping("/{bookListId}/{favoriteId}")
+    public ApiResponse<DeleteResponse> delete(@PathVariable Long bookListId,
+                                              @PathVariable Long favoriteId){
+        var dto = service.delete(bookListId, favoriteId);
+        var response = new DeleteResponse(
+            dto.id()
+        );
+
+        return ApiResponse.success(response);
+    }
+
+    record DeleteResponse(
+        Long id
+    ){}
+
+    @GetMapping("/{bookListId}")
+    public ApiResponse<List<GetResponse>> getByBookList(@PathVariable Long bookListId){
+        var dtos = service.getByBookList(bookListId);
+        var response = from(dtos);
+
+        return ApiResponse.success(response);
+    }
+
+    @GetMapping
+    public ApiResponse<List<GetResponse>> getByMember(){
+        var dtos = service.getByMember();
+        var response = from(dtos);
+
+        return ApiResponse.success(response);
+    }
+
+    private List<GetResponse> from(List<ListFavoriteDto> dtos) {
+        return dtos.stream().map(dto ->
+                new GetResponse(
+                    dto.id(),
+                    dto.memberId(),
+                    dto.bookListId()
+                ))
+            .collect(Collectors.toList());
+    }
+
+    record GetResponse(
+        Long id,
+        Long memberId,
+        Long bookListId
     ){}
 }
