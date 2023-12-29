292d212636f1e14870a40dd7c3796e746737a8c4
digital-hamster <waterlove1439@naver.com>
2023-12-29 14:36:44 +0900

    refactor: S3Uploader 수정(1)
    - MultipartFile 타입을 File 타입으로 변경(로컬에 저장됨) 방식에서, InputStream 방식으로 변경(로컬에 파일이 저장되지 않음)
    
    - BookListService: upload, change 이미지 부분 수정
    ㄴ> 기존에 서비스에서 사용하던 try-catch문 제거

--- src/main/java/com/books_recommend/book_recommend/common/util/S3Uploader.java
+++ src/main/java/com/books_recommend/book_recommend/common/util/S3Uploader.java
@@ -2,18 +2,18 @@ package com.books_recommend.book_recommend.common.util;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Optional;
+import java.io.InputStream;
 
 
 @Slf4j
@@ -27,52 +27,33 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-// MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)
-            .orElse(null);
-        return upload(uploadFile, dirName);
+    public String upload(MultipartFile multipartFile, String dirName) {
+        try {
+            var inputStream = multipartFile.getInputStream();
+            var fileName = dirName + "/" + multipartFile.getOriginalFilename();
+            var contentType = multipartFile.getContentType();
+
+            return putS3(inputStream, fileName, contentType);
+        } catch (IOException e) {
+            log.error("S3 업로드 중 오류 발생 dirName: {}, fileName: {}", dirName, multipartFile.getOriginalFilename());
+            throw new RuntimeException("S3 업로드 중 오류 발생", e);
+        }
     }
 
-    private String upload(File uploadFile, String daylongName) {
-        String fileName = daylongName + "/" + uploadFile.getName();
-        log.info(fileName);
-        String uploadImageUrl = putS3(uploadFile, fileName);
+    private String putS3(InputStream inputStream, String dirName, String contentType) {
+        var objMeta = new ObjectMetadata();
+        objMeta.setContentType(contentType);
 
-        removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-
-        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
-    }
-
-    public String putS3(File uploadFile, String fileName) {
         amazonS3Client.putObject(
-            new PutObjectRequest(bucket, fileName, uploadFile)
-                .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+            new PutObjectRequest(bucket, dirName, inputStream, objMeta)
+                .withCannedAcl(CannedAccessControlList.PublicRead)
         );
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        return amazonS3Client.getUrl(bucket, dirName).toString();
     }
 
-    public String getS3(String bucket, String fileName) {
-        return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-
-    private void removeNewFile(File targetFile) {
-        if(targetFile.delete()) {
-            log.info("파일이 삭제되었습니다.");
-        }else {
-            log.info("파일이 삭제되지 못했습니다.");
-        }
-    }
-
-    public Optional<File> convert(MultipartFile file) throws  IOException {
-        File convertFile = new File(file.getOriginalFilename());
-        if(convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-        return Optional.empty();
-    }
-
+//    public String getS3(String bucket, String fileName) {
+//        return amazonS3Client.getUrl(bucket, fileName).toString();
+//    }
 }
+
+
